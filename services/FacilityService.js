092f6f621601548c18b617329989bc577709cca6
Nour E. Sam <68196199+noureldineessam@users.noreply.github.com>
2024-08-01 10:54:03 +0200

    documentation improvements

--- services/FacilityService.js
+++ services/FacilityService.js
@@ -9,25 +9,25 @@ export class FacilityService {
 
     async getAllUserFacilities(userId) {
         try {
-            logger.info('Fetching all reports');
+            logger.info('Fetching all facilities by user', { userId });
             return await this.facilityRepository.findAllByUserId(userId);
         } catch (error) {
-            logger.error('Error fetching reports', { message: error.message, stack: error.stack });
-            throw new Error('Failed to fetch reports');
+            logger.error('Error fetching facilities', { message: error.message, stack: error.stack });
+            throw new Error('Failed to fetch facilities');
         }
     }
 
     async getUserFacilityById(_id, userId) {
         try {
-            logger.info('Fetching report by ID', { _id });
+            logger.info('Fetching facility by ID', { _id });
             const user = await this.userRepository.findById(userId);
             if (!user || !user.facilities.includes(_id)) throw new Error('Unauthorized');
 
             const facility = await this.facilityRepository.findById(_id);
             return facility ? facility : null;
         } catch (error) {
-            logger.error('Error retrieving report by ID', { message: error.message, stack: error.stack });
-            throw new Error(`Failed to retrieve report: ${error.message}`);
+            logger.error('Error retrieving facility by ID', { message: error.message, stack: error.stack });
+            throw new Error(`Failed to retrieve facility: ${error.message}`);
         }
     }
 
@@ -75,14 +75,14 @@ export class FacilityService {
     async deleteUserFacility(_id, userId) {
         try {
             const facility = await this.facilityRepository.findById(_id);
-            if (!facility) throw new Error('Report not found');
+            if (!facility) throw new Error('Facility not found');
             if (facility.userId.toString() !== userId.toString()) throw new Error('Unauthorized');
 
             await this.facilityRepository.delete(_id);
             return true;
         } catch (error) {
-            logger.error('Error deleting report', error);
-            throw new Error('Failed to delete report');
+            logger.error('Error deleting facility', error);
+            throw new Error('Failed to delete facility');
         }
     }
 }
